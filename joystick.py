16c7c20f09cf5a3ebd8f396c8792f4f859ee3719
Petr Viktorin <encukou@gmail.com>
2019-04-20 19:14:12 -0400

    Add keyboard-based movement & rotation

--- joystick.py
+++ joystick.py
@@ -15,6 +15,18 @@ SHORTEN = 4
 window = pyglet.window.Window(width=SIZE,height=SIZE, resizable=True)
 speeds = [0, 0, 0]
 attrs = [0, 0, 0]
+pressed_keys = set()
+
+ROTATION_KEYS = {
+    pyglet.window.key.Q: -50,
+    pyglet.window.key.E: 50,
+}
+MOVEMENT_KEYS = {
+    pyglet.window.key.W: (0, 1),
+    pyglet.window.key.A: (-1, 0),
+    pyglet.window.key.S: (0, -1),
+    pyglet.window.key.D: (1, 0),
+}
 
 label = pyglet.text.Label('0', y=SMALL_R)
 
@@ -62,9 +74,10 @@ def on_draw():
     draw_circle(0, 0, 20)
     for speed, color in zip(speeds, [(1, 0, 0), (0, 1, 0), (0, 0, 1)]):
         gl.glColor4f(0.2, 0.2, 0.2, 1)
-        draw_rect(-MAX_SPEED/SHORTEN, SMALL_R-5, MAX_SPEED/SHORTEN, SMALL_R)
         label.text = str(speed)
         label.draw()
+        speed = _clamp(speed)
+        draw_rect(-MAX_SPEED/SHORTEN, SMALL_R-5, MAX_SPEED/SHORTEN, SMALL_R)
         gl.glColor4f(*color, 1)
         draw_rect(0, SMALL_R-5, speed/SHORTEN, SMALL_R)
         gl.glRotatef(360/3, 0, 0, 1)
@@ -99,6 +112,36 @@ def on_mouse_scroll(x, y, scroll_x, scroll_y):
     set_position()
 
 
+@window.event
+def on_key_press(keycode, mod):
+    pressed_keys.add(keycode)
+    handle_key(keycode)
+
+
+@window.event
+def on_key_release(keycode, mod):
+    pressed_keys.discard(keycode)
+    handle_key(keycode)
+
+
+def handle_key(key):
+    if key in ROTATION_KEYS:
+        attrs[2] = 0
+        for key, speed in ROTATION_KEYS.items():
+            if key in pressed_keys:
+                attrs[2] += speed
+    if key in MOVEMENT_KEYS:
+        x = y = 0
+        angle = 0
+        for key, (dx, dy) in MOVEMENT_KEYS.items():
+            if key in pressed_keys:
+                x += dx
+                y += dy
+        attrs[0] = math.atan2(y, x) + math.tau / 6
+        attrs[1] = 1000 if y or x else 0
+    set_position()
+
+
 def set_xy(x, y):
     x -= window.width/2
     y -= window.height/2
@@ -120,8 +163,17 @@ def set_position():
         speeds[i] = int(math.cos(motor_angle - angle) * distance) + z * 10
 
 
+def _clamp(speed):
+    if speed < -1023:
+        return -1023
+    if speed > 1023:
+        return 1023
+    return speed
+
+
 def send_position(dt=None):
-    sock.sendall(json.dumps(speeds).encode() + b'\n')
+    message = json.dumps([_clamp(s) for s in speeds]).encode() + b'\n'
+    sock.sendall(message)
 
 pyglet.clock.schedule_interval(send_position, 1/10)
 
