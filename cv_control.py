e23db6fe606ec6c766532f33296babc5ef854648
Petr Viktorin <encukou@gmail.com>
2019-04-21 16:00:25 -0400

    Some clean-up

--- cv_control.py
+++ cv_control.py
@@ -142,11 +142,14 @@ def calibrate():
 
 
 class Object2D:
-    def __init__(self, corners):
+    def __init__(self, corners, *, angle=None):
         self.center = corners.sum(0) / 4
         y1 = corners[:2].sum(0) / 2
-        self.angle = math.atan2(y1[1]-self.center[1], y1[0]-self.center[0]) + math.tau/4
-        self.size = np.linalg.norm(self.center - y1)
+        if angle is None:
+            self.angle = math.atan2(y1[1]-self.center[1], y1[0]-self.center[0]) + math.tau/4
+        else:
+            self.angle = angle
+        self.size = np.linalg.norm(self.center - y1) * 2
 
         c, s = np.cos(-self.angle), np.sin(-self.angle)
         self.rotation_matrix = np.array(((c, -s), (s, c)))
@@ -164,52 +167,47 @@ def drive():
         f = Path('calibration.npz').open('rb')
     except FileNotFoundError:
         camera_matrix = dist_coeffs = None
+        drawing = False
     else:
         with f:
             npz = np.load(f)
             camera_matrix = npz['camera_matrix']
             dist_coeffs = npz['dist_coeffs']
-    print(camera_matrix)
+        drawing = True
 
     cap = cv2.VideoCapture(0)
     board = _get_board()
 
-    circ = np.float32([[0, 0, 0], [3,0,0], [0,3,0], [0,0,-3]]).reshape((-1, 1, 3))
-    CIRC_N = 64
-    circ = np.zeros((CIRC_N*2+7, 1, 3))
-    for i in range(CIRC_N):
-        circ[i*2][0][0] = math.cos(math.tau/CIRC_N*i) * 2 / 3
-        circ[i*2][0][1] = math.sin(math.tau/CIRC_N*i) * 2 / 3
-        circ[i*2+1][0][0] = math.cos(math.tau/CIRC_N*(i+0.5)) * 2 / 3
-        circ[i*2+1][0][1] = math.sin(math.tau/CIRC_N*(i+0.5)) * 2 / 3
-        circ[i*2+1][0][2] = 1/4
+    diagram_points = np.zeros((7, 1, 3))
     for i in range(3):
         c = math.cos(math.tau/3*(-i-1))
         s = math.sin(math.tau/3*(-i-1))
-        circ[CIRC_N*2+1+i*2][0][0] = -c - s
-        circ[CIRC_N*2+1+i*2][0][1] = s - c
-        circ[CIRC_N*2+2+i*2][0][0] = c - s
-        circ[CIRC_N*2+2+i*2][0][1] = -s - c
+        diagram_points[i*2][0][0] = -c - s
+        diagram_points[i*2][0][1] = s - c
+        diagram_points[i*2+1][0][0] = c - s
+        diagram_points[i*2+1][0][1] = -s - c
 
     target = None
     def set_target(event,x,y,flags,param):
         nonlocal target
         if event == cv2.EVENT_LBUTTONDOWN:
-            print('*'*99)
-            target = Object2D(np.array([[x-10, y-10], [x+10, y+10], [x-10, y+10], [x+10, y+10]]))
+            array = np.array([[x-10, y-10], [x+10, y+10], [x-10, y+10], [x+10, y+10]])
+            if flags & cv2.EVENT_FLAG_CTRLKEY:
+                target = Object2D(array, angle=90)
+            else:
+                target = Object2D(array)
     cv2.namedWindow('frame')
     cv2.setMouseCallback('frame', set_target)
 
     while True:
         ret, frame = cap.read()
         #frame = cv2.imread('scsh.jpg')
-        #frame = cv2.imread('scsh.png')
+        frame = cv2.imread('scsh.png')
         h,  w = frame.shape[:2]
-        #newcameramtx, roi=cv2.getOptimalNewCameraMatrix(camera_matrix, dist_coeffs, (w,h), 0, (w,h))
-        #frame = cv2.undistort(frame, camera_matrix, dist_coeffs, None, newcameramtx)
 
-        #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        #n, frame = cv2.threshold(frame, 200, 200, cv2.THRESH_BINARY)
+        if drawing:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR)
 
         key = cv2.waitKey(1)
         if key & 0xFF == ord('q'):
@@ -217,10 +215,12 @@ def drive():
         if key & 0xFF == ord('s'):
             cv2.imwrite('scsh.jpg', frame)
 
-        #cv2.circle(frame, (w//2, h//2), 20, (0, 200, 255), -1)
+        if target is None:
+            target = Object2D(np.array([[0, 0], [w, 0], [0, h], [w, h]]))
+        if drawing:
+            target.draw(frame)
 
         corners, ids, rejected = cv2.aruco.detectMarkers(frame, board.dictionary)
-        #ids = None
         if ids is not None:
             diamond_corners, diamond_ids = cv2.aruco.detectCharucoDiamond(
                 frame, corners, ids, 1/0.6)
@@ -228,23 +228,26 @@ def drive():
             for corner, color in zip(diamond_corners, ((0,f,0), (f,0,0), (0,0,f), (f,f,0), (0,f,f), (f,0,f))):
                 if diamond_ids is not None:
                     car = Object2D(corner.reshape((4, 2)))
-                    car.draw(frame, color)
-                    if target is None:
-                        target = Object2D(np.array([[0, 0], [w, 0], [0, h], [w, h]]))
-                    target.draw(frame)
+                    if drawing:
+                        car.draw(frame, color)
+
+                    ANGLE_SATURATION = 0.7  # rad
+                    DISTANCE_SATURATION = 4  # charuco tiles
+                    DISTANCE_PRIORITY = 2
+                    MOTOR_MAX = 1023
 
                     angle_diff = target.angle - car.angle
                     while angle_diff > math.tau/2:
                         angle_diff -= math.tau
                     while angle_diff < -math.tau/2:
                         angle_diff += math.tau
-                    if angle_diff > 0.7:
-                        angle_diff = 0.7
-                    if angle_diff < -0.7:
-                        angle_diff = -0.7
+                    if angle_diff > ANGLE_SATURATION:
+                        angle_diff = ANGLE_SATURATION
+                    if angle_diff < -ANGLE_SATURATION:
+                        angle_diff = -ANGLE_SATURATION
 
                     drive_vector = target.center - car.center
-                    drive_vector /= car.size * 3
+                    drive_vector /= car.size * DISTANCE_SATURATION
                     drive_vector = car.rotation_matrix @ drive_vector
                     drive_vector *= 1, -1
                     norm = np.linalg.norm(drive_vector)
@@ -252,62 +255,47 @@ def drive():
                         drive_vector /= norm
                         norm = 1
 
-                    print('D', drive_vector, car.angle)
-                    frame = cv2.line(
-                        frame,
-                        tuple(int(c) for c in drive_vector*[1,-1]*50 + [w//2, h//2]),
-                        (w//2, h//2),
-                        color, 1,
-                    )
+                    if drawing:
+                        frame = cv2.line(
+                            frame,
+                            tuple(int(c) for c in drive_vector*[1,-1]*50 + [w//2, h//2]),
+                            (w//2, h//2),
+                            color, 1,
+                        )
 
                     speeds = np.zeros(3)
                     for i in range(3):
-                        distance = norm
                         angle = math.atan2(drive_vector[1], drive_vector[0]) + math.tau/6
                         motor_angle = math.tau / 3 * i
                         speeds[i] = int(
-                            math.cos(motor_angle - angle) * distance * 1000
-                            + angle_diff * 1000
+                            math.cos(motor_angle - angle) * norm * MOTOR_MAX * DISTANCE_PRIORITY
+                            + angle_diff * MOTOR_MAX / ANGLE_SATURATION
                         )
                     mx = max(speeds)
-                    if mx > 1000:
-                        speeds = speeds / mx * 1000
+                    if mx > MOTOR_MAX:
+                        speeds = speeds / mx * MOTOR_MAX
                     mn = min(speeds)
-                    if mn < -1000:
-                        speeds = speeds / mn * -1000
-                    print(speeds)
+                    if mn < -MOTOR_MAX:
+                        speeds = speeds / mn * -MOTOR_MAX
 
-                    ### DRAWING
-
-                    if camera_matrix is not None:
+                    if drawing:
                         rvec, tvec, points = cv2.aruco.estimatePoseSingleMarkers(
                             [corner], 1, camera_matrix, dist_coeffs,
                         )
-                        #cv2.aruco.drawAxis(frame, camera_matrix, dist_coeffs, r[0], t[0], 1)
 
                         for r, t, ids in zip(rvec, tvec, diamond_ids):
 
-                            pts, proj = cv2.projectPoints(circ, r, t, camera_matrix, dist_coeffs)
+                            pts, proj = cv2.projectPoints(
+                                np.array([[[drive_vector[0], drive_vector[1], 0]], [[0, 0, 0]]]),
+                                r, t, camera_matrix, dist_coeffs,
+                            )
+                            frame = cv2.polylines(frame, [pts.astype(int)], True, (140, 100, 100), 5)
 
-                            # G R
-                            #  B
-
-                            #print(r, t, ids)
-                            center = pts[-7][0]
-                            to_x = pts[0][0]
-                            diff = to_x - center
-                            rot = -math.atan2(diff[1], diff[0])
-                            rot = angle_diff
-                            rot = 0
-                            print(rot)
-                            if rot < -1:
-                                rot = -1
-                            if rot > 1:
-                                rot = 1
+                            pts, proj = cv2.projectPoints(diagram_points, r, t, camera_matrix, dist_coeffs)
 
                             for i, color in enumerate(((0, 0, 255), (0, 255, 0), (255, 0, 0))):
-                                a = pts[CIRC_N*2+1+i*2][0]
-                                b = pts[CIRC_N*2+2+i*2][0]
+                                a = pts[i*2][0]
+                                b = pts[i*2+1][0]
                                 frame = cv2.line(
                                     frame,
                                     tuple(int(c) for c in a),
@@ -330,22 +318,22 @@ def drive():
                                     color, 5,
                                 )
 
-                            frame = cv2.polylines(frame, [pts[:CIRC_N*2].astype(int)], True, (255, 255, 255), 3)
+                                for color, size in ((0, 0, 0), 2), ((255, 255, 255), 1):
+                                    cv2.putText(
+                                        frame, str(int(speeds[i])),
+                                        tuple(int(c) for c in mid),
+                                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, size, cv2.LINE_AA,
+                                    )
 
                             cv2.putText(
                                 frame, str(ids[0]),
-                                tuple(int(c) for c in pts[-7][0]),
-                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1, cv2.LINE_AA,
+                                tuple(int(c) for c in pts[-1][0]),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1, cv2.LINE_AA,
                             )
 
-                            pts, proj = cv2.projectPoints(
-                                np.array([[[drive_vector[0], drive_vector[1], 0]], [[0, 0, 0]]]),
-                                r, t, camera_matrix, dist_coeffs,
-                            )
-                            frame = cv2.polylines(frame, [pts.astype(int)], True, (140, 100, 100), 5)
 
-
-        cv2.imshow('frame', frame)
+        if drawing:
+            cv2.imshow('frame', frame)
 
     cap.release()
     cv2.destroyAllWindows()
